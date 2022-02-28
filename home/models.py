1ea8995dbe2b2f62fc4dd417beda2cf6a7410b63
mslmyrtd <mslmyrtd@gmail.com>
2022-02-28 14:46:31 +0300

    models.py update

--- home/models.py
+++ home/models.py
@@ -6,4 +6,5 @@ class Contact(models.Model):
     phone_number=models.CharField(max_length=40)
     email=models.EmailField(max_length=254)
     message=models.TextField()
-    
+    def __str__(self):
+        return self.name
