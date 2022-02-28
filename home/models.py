7505b1ce8802010b162ba4f614b986ba20e974d8
mslmyrtd <mslmyrtd@gmail.com>
2022-02-28 18:33:36 +0300

    branch and teacher models added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,20 @@ class Contact(models.Model):
     message=models.TextField()
     def __str__(self):
         return self.name
+    
+class Branch(models.Model):
+    branch_name=models.CharField(max_length=54)
+    createdDate=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.branch_name
+class Teacher(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    phone_teacher=models.CharField(max_length=100)
+    email_teacher=models.EmailField(max_length=254)
+    branch=models.ForeignKey(Branch, on_delete=models.PROTECT)
+    image=models.ImageField(upload_to="teacher", default="default.png")
+    createdDate=models.DateTimeField(auto_now_add=True)
+    updateDate=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.first_name
