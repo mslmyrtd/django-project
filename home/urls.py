6697b33f3e93f72b823921d1cfd1cf61d27bee28
mslmyrtd <mslmyrtd@gmail.com>
2022-02-28 16:42:56 +0300

    url eklendi update edildi

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home,teacher,about
 
 
 urlpatterns = [
     path("", home, name="home"),
+    path("about/", about, name="about"),
+    path("teacher/", teacher, name="teacher"),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,"home/index.html")
+def teacher(request):
+    return render(request,"home/teacher.html")
+def about(request):
+    return render(request,"home/about.html")
