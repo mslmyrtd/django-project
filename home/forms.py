08f27943a821e79d9bdf10dd47ee07f235b69bc9
mslmyrtd <mslmyrtd@gmail.com>
2022-02-28 18:20:02 +0300

    widget added forms changed

--- home/forms.py
+++ home/forms.py
@@ -6,3 +6,13 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model=Contact
         fields="__all__"
+        
+        widgets = {
+            "name": forms.TextInput(attrs={'placeholder':'Name'}),
+            "phone_number": forms.TextInput(attrs={'placeholder':'Phone Number'}),
+            "email": forms.TextInput(attrs={'placeholder':'Email'}),
+            "message": forms.TextInput(attrs={'placeholder':'Message'}),
+        }
+        
+        
+    
--- home/views.py
+++ home/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .forms import ContactForm
 # Create your views here.
 
 def home(request):
-    return render(request,"home/index.html")
+    form=ContactForm()
+    if request.method=="POST":
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context={
+        "form":form
+    }
+    return render(request,"home/index.html",context)
 def teacher(request):
     return render(request,"home/teacher.html")
 def about(request):
     return render(request,"home/about.html")
+
