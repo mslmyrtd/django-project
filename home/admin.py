e2c3cbd4827cb4c87ee275b66fcae3c59d638802
mslmyrtd <mslmyrtd@gmail.com>
2022-02-28 18:48:48 +0300

    new teacher added

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact,Teacher,Branch
 # Register your models here.
 
 
@@ -10,3 +10,12 @@ class ContactAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Contact, ContactAdmin)
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display=["first_name","email_teacher","branch"]
+    list_filter=("first_name",)
+    search_filter=("first_name__startswith",)
+    
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Branch)
