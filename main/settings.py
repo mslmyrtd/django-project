b1e4aac2280747b1d0571b8e9abbb791234fc803
mslmyrtd <mslmyrtd@gmail.com>
2022-03-01 14:49:31 +0300

    login logout codları yazıldı

--- main/settings.py
+++ main/settings.py
@@ -143,4 +143,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CRISPY_TEMPLATE_PACK="bootsrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL="/"
